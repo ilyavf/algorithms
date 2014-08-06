270808fc8ddc25513ca8dd94feedc8d4c128963f
Ilya Fadeev <fadeev.ilya@gmail.com>
2014-08-06 15:39:49 -0400

    minor update for palindrome

--- src/palindrome.js
+++ src/palindrome.js
@@ -15,8 +15,8 @@ function getLargestP (s) {
     var s1 = s.substr(0, s.length-1),
         s2 = s.slice(1),
 
-        l1 = s1 && getLargestP(s1),
-        l2 = s2 && getLargestP(s2);
+        l1 = getLargestP(s1),
+        l2 = getLargestP(s2);
 
     return l1 && l2 && (l1.length >= l2.length ? l1 : l2)
         || l1
